3aa8748d2a7645fc89ab6c2c733bb1fc82098cdf
longtq.aht-asia.fe <longtq@aht-asia.com>
2024-06-10 11:41:02 +0700

    clear

--- products/serializers.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth import get_user_model
-from rest_framework import serializers
-from .models import Product, ProductCategory
-
-class ProductSerializer(serializers.Serializer):
-    class Meta: 
-        model = Product
-        fields =["product_id", "name", "price", "img_url", "description", "created", "updated"]
